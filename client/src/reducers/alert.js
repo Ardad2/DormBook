0c0ef020d6c39c9544749a4de9358ec6c07d370d
Arjun Dadhwal <arjund2002@gmail.com>
2021-07-06 03:30:00 +0530

    create reducer for alerts

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,8 +1,16 @@
+import { SET_ALERT, REMOVE_ALERT } from "../actions/types";
+
 const initialState = [];
 
 export default function (state = initialState, action) {
-    switch(action.type) {
-        case 'SET_ALERT'
+  const { type, payload } = action;
 
-    }
+  switch (type) {
+    case SET_ALERT:
+      return [...state, payload]; //Add new alert to already exisitng alerts (state)
+    case REMOVE_ALERT:
+      return state.filter((alert) => alert.id !== payload);
+    default:
+      return state;
+  }
 }
