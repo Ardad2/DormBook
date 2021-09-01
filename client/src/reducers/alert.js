81cdb9fd5895607a8167d36035705d44bd459c23
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2021-09-01 16:20:52 -0700

    fixed alert reducer

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -6,6 +6,29 @@ export default function (state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
+    case USER_LOADED: 
+    return {
+      ...state,
+      isAuthenticated: true,
+      loading: false,
+      user: payload
+    }
+    case REGISTER_SUCCESS:
+      localStorage.setItem("token", payload.token)
+      return {
+        ...state, 
+        ...payload,
+        isAuthenticated: true,
+        loading: false
+      };
+      case REGISTER_FAIL:
+        localStorage.removeItem('token');
+        return {
+          ...state,
+          token: null, 
+          isAuthenticated: false,
+          loading: false
+        }
     case SET_ALERT:
       return [...state, payload]; //Add new alert to already exisitng alerts (state)
     case REMOVE_ALERT:
