8ef536e62732a4334cc525a13857a74f6f14cfec
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2021-09-05 21:51:33 -0700

    Created login user request to API

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { setAlert } from './alert'
 import { 
   REGISTER_SUCCESS,
-  REGISTER_FAIL, USER_LOADED, AUTH_ERROR
+  REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL
 } from './types';
 import setAuthToken from "../utils/setAuthToken";
 
@@ -63,6 +63,45 @@ export const register = ({ name, email, password}) => async dispatch =>
     dispatch(
       {
         type: REGISTER_FAIL
+      }
+    );
+  }
+
+}
+
+// Login user 
+export const login = ({ email, password}) => async dispatch =>
+{
+  const config = 
+  {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  const body = JSON.stringify({email, password});
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+
+    dispatch(
+      {
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+  }
+  catch (err)
+  {
+    const errors = err.response.data.errors;
+
+    if (errors)
+    {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch(
+      {
+        type: LOGIN_FAIL
       }
     );
   }
