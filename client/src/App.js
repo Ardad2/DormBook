2df5f5313864c3bf71929ec3206b0bcdda7d180a
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2021-09-13 14:06:22 -0700

    Made dashboard route private

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 import setAuthToken from "./utils/setAuthToken";
 import "./App.css";
 import { Provider } from "react-redux";
@@ -37,7 +38,7 @@ const App = () => {
           <Switch>
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
-            <Route exact path="/Dashboard" component={Dashboard} />
+            <PrivateRoute exact path="/Dashboard" component={Dashboard} />
           </Switch>
         </section>
       </Fragment>
