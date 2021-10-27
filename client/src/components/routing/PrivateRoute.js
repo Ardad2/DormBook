5ee1fe5d92c81be924f32a76b4cd5d40824e9bcd
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2021-10-26 22:40:08 -0700

    finish profile

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
 
-
-const PrivateRoute = (
-    { component: Component, 
-        auth: {isAuthenticated, loading}, 
-        ...rest 
-    }) => (
-    <Route 
+const PrivateRoute = ({
+  component: Component,
+  auth: { isAuthenticated, loading },
+  ...rest
+}) => (
+  <Route
     {...rest}
-    render={props =>  !isAuthenticated ? (<Redirect to='/login' />) : (<Component {...props} />)
-}
-/>
+    render={props =>
+      loading ? (
+        <Spinner />
+      ) : isAuthenticated ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
 );
 
 PrivateRoute.propTypes = {
-    auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-    auth: state.auth
+  auth: state.auth
 });
-
 
 export default connect(mapStateToProps)(PrivateRoute);
